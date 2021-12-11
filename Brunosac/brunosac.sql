45a87c2796030d442dbc7067287037b803adf1cc
unknown <diegojp20@gmail.com>
2021-12-11 00:25:25 -0500

    Proyecto-1.1

--- Brunosac/brunosac.sql
+++ Brunosac/brunosac.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-11-2021 a las 03:33:48
+-- Tiempo de generación: 11-12-2021 a las 06:18:45
 -- Versión del servidor: 10.4.17-MariaDB
 -- Versión de PHP: 8.0.0
 
@@ -30,18 +30,19 @@ SET time_zone = "+00:00";
 CREATE TABLE `cliente` (
   `idCliente` int(11) NOT NULL,
   `razonSocial` varchar(200) NOT NULL,
-  `ruc` bigint(20) NOT NULL
+  `ruc` bigint(20) NOT NULL,
+  `direccion` varchar(1000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `cliente`
 --
 
-INSERT INTO `cliente` (`idCliente`, `razonSocial`, `ruc`) VALUES
-(1, 'REPRESENTACIONES DIERA S.A.C.', 20553476462),
-(2, 'MODAS LOREN EIRL', 20543248984),
-(3, 'BI GRAND CONFECCIONES S.A.C.', 20553856451),
-(4, 'ARTROSCOPICTRAUMA S.A.C.', 20538856674);
+INSERT INTO `cliente` (`idCliente`, `razonSocial`, `ruc`, `direccion`) VALUES
+(1, 'REPRESENTACIONES DIERA S.A.C.', 20553476462, ' Av Javier Prado Oeste, 595 Magdalena del Mar, Cercado de Lima 15076'),
+(2, 'MODAS LOREN EIRL', 20543248984, 'Av. dos de Mayo 1002, San Isidro 15073'),
+(3, 'BI GRAND CONFECCIONES S.A.C.', 20553856451, 'Av. Mariscal Andres de Sta. Cruz 898, Miraflores 15073'),
+(4, 'ARTROSCOPICTRAUMA S.A.C.', 20538856674, 'Av. Gral. Salaverry 694, Jesús María 15072');
 
 -- --------------------------------------------------------
 
@@ -64,17 +65,17 @@ CREATE TABLE `entradaproducto` (
 --
 
 INSERT INTO `entradaproducto` (`idEntProd`, `fecha`, `idRegPro`, `descripcion`, `cantidad`, `idProveedor`, `numeroFactura`) VALUES
-(27, '2021-10-13', 1, 'motor de 500w', 5, 2, '0000001'),
+(27, '2021-10-12', 1, 'motor de 500w', 5, 2, '0000001'),
 (29, '2021-10-13', 11, 'area de soporte', 2, 3, '0000002'),
 (30, '2021-11-02', 11, 'cable calidad 30', 8, 2, '0000003'),
-(31, '2021-10-10', 10, 'area de soporte', 5, 3, '0000004'),
-(32, '2021-10-10', 13, 'recepcion', 6, 1, '0000005'),
-(33, '2021-10-10', 13, 'area electrica', 4, 4, '0000006'),
-(34, '2021-10-18', 9, 'demo', 4, 3, '0000007'),
-(35, '2021-11-07', 1, 'oficina', 8, 1, '0000008'),
-(36, '2021-11-07', 9, 'oficina 2C', 10, 4, '0000009'),
-(37, '2021-11-07', 1, 'Almace 2B', 11, 1, '00000010'),
-(38, '2021-11-08', 14, 'Lorem Ipsum is simply dummy text of the printing and typesetting', 10, 3, '00000011');
+(31, '2021-11-10', 10, 'area de soporte', 5, 3, '0000004'),
+(32, '2021-11-10', 13, 'recepcion', 6, 1, '0000005'),
+(33, '2021-11-10', 13, 'area electrica', 4, 4, '0000006'),
+(34, '2021-11-18', 9, 'demo', 4, 3, '0000007'),
+(35, '2021-11-27', 1, 'oficina', 8, 1, '0000008'),
+(36, '2021-11-28', 9, 'oficina 2C', 10, 4, '0000009'),
+(37, '2021-11-29', 1, 'Almace 2B', 11, 1, '00000010'),
+(38, '2021-12-01', 14, 'Lorem Ipsum is simply dummy text of the printing and typesetting', 10, 3, '00000011');
 
 -- --------------------------------------------------------
 
@@ -99,11 +100,11 @@ INSERT INTO `inventario` (`idInventario`, `codigo`, `nombre`, `precio`, `stock`)
 (2, 'R01', 'Roscadora modelo 535', 23.5, 85),
 (3, 'M02', 'Máquina automática de fusión a tope V160 CNC 50MM-160MM', 30.5, 17),
 (4, 'M03', 'Máquina de fusión a tope hidráulica V160 BEE 50MM-160MM', 16.5, 18),
-(5, 'J01', 'Juego Peines Tarrajas Truper 13083 1-1/2', 18.5, 55),
+(5, 'J01', 'Juego Peines Tarrajas Truper 13083 1-1/2', 18.5, 51),
 (6, 'P01', 'Peine Ridgid Para Tarraja 1 Npt R12', 17.5, 80),
 (7, 'A01', 'Anillos y mordazas de la serie estándar para MegaPress® de ½” a 2”', 40.5, 45),
 (8, 'M04', 'Mordazas compactas para MegaPress® de ½ in - 1 in', 15.5, 35),
-(9, 'M05', 'Máquina para fregaderos K-40', 45, 80),
+(9, 'M05', 'Máquina para fregaderos K-40', 45, 79),
 (10, 'C01', 'Cortatubos Press Snap™ para tubo de suelo', 60, 52),
 (11, 'L01', 'Limpiador de chorro de agua KJ-3100', 30, 12),
 (13, 'R02', 'Recortacables ACSR Modelo 87', 66, 70),
@@ -186,14 +187,16 @@ CREATE TABLE `salidas` (
 --
 
 INSERT INTO `salidas` (`idSalida`, `fecha`, `idRegPro`, `descripcion`, `cantidad`, `idCliente`, `numeroFactura`) VALUES
-(1, '2021-10-17', 8, 'cliente MODAS', 5, 2, '0000001'),
+(1, '2021-10-17', 8, 'cliente, MODAS', 5, 2, '0000001'),
 (2, '2021-10-17', 6, 'cliente BI GRAND', 4, 3, '0000002'),
 (3, '2021-10-18', 7, 'cliente Artros', 5, 4, '0000003'),
 (7, '2021-10-18', 9, 'cliente Artro', 5, 4, '0000004'),
 (8, '2021-11-07', 1, 'Metro', 4, 1, '0000005'),
 (9, '2021-11-07', 8, 'demos', 7, 1, '0000006'),
 (10, '2021-11-08', 14, 'Lorem Ipsum is simply dummy text of the printing and typesetting', 5, 2, '0000007'),
-(11, '2021-11-08', 14, 'Lorem Ipsum is simply dummy text of the printing and typesetting', 8, 1, '0000008');
+(11, '2021-11-08', 14, 'Lorem Ipsum is simply dummy text of the printing and typesetting', 8, 1, '0000008'),
+(12, '2021-12-05', 5, 'demo', 4, 1, '0000009'),
+(13, '2021-12-10', 9, 'demo 2', 1, 1, '00000010');
 
 -- --------------------------------------------------------
 
@@ -302,7 +305,7 @@ ALTER TABLE `registrpproducto`
 -- AUTO_INCREMENT de la tabla `salidas`
 --
 ALTER TABLE `salidas`
-  MODIFY `idSalida` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `idSalida` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
